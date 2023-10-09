cbef7c4d0c94a8f5e916aabbfd8fec456b4b69b8
jakaria242 <jakaria.dev242@gmail.com>
2023-10-09 09:14:31 +0600

    petroil project done

---
+++ js/tailwind.config.js
@@ -0,0 +1,34 @@
+
+
+
+
+    tailwind.config = {
+      theme: {
+        extend: {
+          colors: {
+            red : '#F40404',
+            headerbg : '#282828',
+            line : '#5C6A92',
+            yellow : '#FFB800',
+            supplierbg : '#E5E5E5',
+            black : '#000000',
+            peragraph : '#6C6C6C',
+            'overlay-bg': "rgba(0, 0, 0, 0.60)",
+            company : '#F0F0F0',
+            blogbtn : 'rgba(255, 255, 255, 0.50)',
+            footerbg : '#1F1F1F',
+            lastbg : '#282828',
+          },
+        maxWidth: {
+             'container': '1140px',
+          },
+        fontFamily: {
+            'montserrat': "'Montserrat', 'sans-serif'",
+            'poppins': "'Poppins', 'sans-serif'",
+         },
+        backgroundImage: {
+            'bannerImg': "linear-gradient(rgba(0, 0, 0, 0.60), rgba(0, 0, 0, 0.60)),url('images/banner.jpg')",
+          },
+        }
+      }
+    }
